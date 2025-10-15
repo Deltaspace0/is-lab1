dbb89e84f0d321b28af166d2b76dcf82427de88e
Ruslan Gadeev <rgadeew@yandex.ru>
2025-10-15 19:56:21 +0300

    build: add Vite+React

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: 'src/main/js',
+  build: {
+    outDir: '../../../dist'
+  },
+  server: {
+    proxy: {
+      '/person': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
+});
