cb5b5b546df519d84bd69d0d722be80c3a267389
Ruslan Gadeev <rgadeew@yandex.ru>
2025-11-10 13:16:02 +0300

    fix: simultaneous import of the same file

---
+++ src/main/java/com/deltaspace/lab/service/FileService.java
@@ -0,0 +1,39 @@
+package com.deltaspace.lab.service;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.deltaspace.lab.model.Person;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Service
+public class FileService {
+
+    private final PersonService personService;
+
+    public FileService(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public Integer processFile(
+        MultipartFile file,
+        String username
+    ) throws IOException {
+        String json = new String(file.getBytes(), StandardCharsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        Person[] persons = mapper.readValue(json, Person[].class);
+        for (int i = 0; i < persons.length; i++) {
+            persons[i] = personService.add(persons[i]);
+        }
+        return persons.length;
+    }
+
+}
--- src/main/java/com/deltaspace/lab/service/ImportService.java
+++ src/main/java/com/deltaspace/lab/service/ImportService.java
@@ -1,7 +1,5 @@
 package com.deltaspace.lab.service;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
@@ -9,45 +7,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.deltaspace.lab.model.ImportData;
-import com.deltaspace.lab.model.Person;
 import com.deltaspace.lab.repository.ImportRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
-import jakarta.transaction.Transactional;
 
 @Service
 public class ImportService {
 
     private final ImportRepository importRepository;
-    private final PersonService personService;
+    private final FileService fileService;
 
     public ImportService(
         ImportRepository importRepository,
-        PersonService personService
+        FileService fileService
     ) {
         this.importRepository = importRepository;
-        this.personService = personService;
-    }
-
-    @Transactional
-    private Integer processFile(
-        MultipartFile file,
-        String username
-    ) throws IOException {
-        String json = new String(file.getBytes(), StandardCharsets.UTF_8);
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        Person[] persons = mapper.readValue(json, Person[].class);
-        for (Person person : persons) {
-            personService.add(person);
-        }
-        return persons.length;
+        this.fileService = fileService;
     }
 
     public Integer importFile(MultipartFile file, String username) {
         try {
-            Integer count = processFile(file, username);
+            Integer count = fileService.processFile(file, username);
             ImportData data = new ImportData(true, username, count);
             importRepository.save(data);
             return count;
